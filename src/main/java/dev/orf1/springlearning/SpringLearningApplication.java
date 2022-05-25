9957ca1d8d05903a1c99bd94d488ee1506353062
Orf1 <orfeas126@gmail.com>
2022-05-25 08:13:36 -0700

    Changed stuff

--- src/main/java/dev/orf1/springlearning/SpringLearningApplication.java
+++ src/main/java/dev/orf1/springlearning/SpringLearningApplication.java
@@ -2,19 +2,23 @@ package dev.orf1.springlearning;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+
+@Configuration
+@ComponentScan
 public class SpringLearningApplication {
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearningApplication.class);
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(SpringLearningApplication.class, args);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringLearningApplication.class);
 
         ComponentDAO componentDAO = context.getBean(ComponentDAO.class);
 
         LOGGER.info("componentDAO instance: {}", componentDAO);
+
+        context.close();
     }
 }
